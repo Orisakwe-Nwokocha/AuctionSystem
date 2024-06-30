15abd986662d2b417093594318180d9c191ceb45
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-30 11:43:48 +0100

    updated auction and bid models

--- src/main/java/com/example/auction_system/data/models/Auction.java
+++ src/main/java/com/example/auction_system/data/models/Auction.java
@@ -1,4 +1,64 @@
 package com.example.auction_system.data.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.time.LocalDateTime.now;
+
+@Getter
+@Setter
+@Entity
+@ToString
 public class Auction {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+//    @OneToOne
+//    private Seller seller;
+//    @OneToOne
+//    private Product product;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal baseBid;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal highestBid;
+
+    @Setter(AccessLevel.NONE)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime expiryDate;
+
+    @Setter(AccessLevel.NONE)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime dateCreated;
+
+    @Setter(AccessLevel.NONE)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime dateUpdated;
+
+
+    @PrePersist
+    private void setDateCreated(){
+        dateCreated = now();
+
+    }
+
+    @PreUpdate
+    private void setDateUpdated(){
+        dateUpdated = now();
+    }
 }
--- src/main/java/com/example/auction_system/data/models/Bid.java
+++ src/main/java/com/example/auction_system/data/models/Bid.java
@@ -1,4 +1,50 @@
 package com.example.auction_system.data.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.time.LocalDateTime.now;
+
+@Entity
+@Getter
+@Setter
+@ToString
 public class Bid {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Bidder bidder;
+
+    @ManyToOne
+    private Auction auction;
+
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal amount;
+
+    @Setter(AccessLevel.NONE)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime bidTime;
+
+    @PrePersist
+    private void setBidTime(){
+        bidTime = now();
+
+    }
+
+
 }
