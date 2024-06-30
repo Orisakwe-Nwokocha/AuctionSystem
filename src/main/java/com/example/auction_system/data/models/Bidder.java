7b720b9fbcaf10a3761b4a6d89cd5ebb3f585ddc
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-06-30 09:10:20 +0100

    populated the bidder and biodata models and added the uml diagrams for the app

--- src/main/java/com/example/auction_system/data/models/Bidder.java
+++ src/main/java/com/example/auction_system/data/models/Bidder.java
@@ -1,4 +1,21 @@
 package com.example.auction_system.data.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "bidders")
+@Getter
+@Setter
 public class Bidder {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "bio_data_id")
+    private BioData bioData;
+
 }
--- src/main/java/com/example/auction_system/data/models/BioData.java
+++ src/main/java/com/example/auction_system/data/models/BioData.java
@@ -1,4 +1,50 @@
 package com.example.auction_system.data.models;
 
+import com.example.auction_system.data.constants.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.time.LocalDateTime.now;
+import static lombok.AccessLevel.NONE;
+
+@Entity
+@Table(name = "bio_data")
+@Getter
+@Setter
 public class BioData {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    @Enumerated(STRING)
+    private Role role;
+    @Setter(NONE)
+    private LocalDateTime dateRegistered;
+    @Setter(NONE)
+    private LocalDateTime dateUpdated;
+
+    @OneToOne(mappedBy = "bioData", cascade = ALL)
+    private Bidder bidder;
+
+//    @OneToOne(mappedBy = "bioData", cascade = ALL)
+//    private Seller seller;
+
+    @PrePersist
+    private void setDateRegistered() {
+        dateRegistered = now();
+    }
+
+    @PreUpdate
+    private void setDateUpdated() {
+        dateUpdated = now();
+    }
+
 }
